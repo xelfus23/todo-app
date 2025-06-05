83321281aa4a0ec78b047194fac05555b3212e29
VC-PC1 <xelfus.dev@gmail.com>
2025-06-05 16:51:55 +0800

    second commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,103 +1,122 @@
-import Image from "next/image";
+"use client";
+import { dummyTodoItems } from "@/constants/todoDummy";
+import { useState } from "react";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const buttonFunctions = [
+        {
+            name: "Delete",
+            color: "bg-red-400",
+        },
+        {
+            name: "Edit",
+            color: "bg-blue-400",
+        },
+        {
+            name: "Complete",
+            color: "bg-green-400",
+        },
+    ];
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    const [formValue, setFormValue] = useState({
+        task: "",
+        category: "",
+        dueDate: "",
+        priority: "",
+    });
+
+    return (
+        <div className="grid grid-cols-5">
+            <div className="h-screen">
+                <div className="border-r rounded-md h-full p-2">
+                    <form className="space-y-4">
+                        {Object.keys(formValue).map((field, index) => {
+                            return (
+                                <div
+                                    key={index}
+                                    className="space-x-2 flex flex-col"
+                                >
+                                    <label className="font-bold">
+                                        {field === "dueDate"
+                                            ? "Due Date"
+                                            : field.charAt(0).toUpperCase() +
+                                              field.slice(1)}
+                                    </label>
+                                    <input
+                                        className="border rounded-md p-2 outline-none"
+                                        onChange={(e) => {
+                                            setFormValue((prev) => {
+                                                return {
+                                                    ...prev,
+                                                    [e.target.name]:
+                                                        e.target.value,
+                                                };
+                                            });
+                                        }}
+                                    />
+                                </div>
+                            );
+                        })}
+                    </form>
+                </div>
+            </div>
+            <div className="h-screen bg-gray-950 grid grid-cols-2 p-4 space-x-4 col-span-4">
+                <div className="border p-4 rounded-xl space-y-4">
+                    <div className="flex justify-between">
+                        <h1 className="text-2xl font-bold text-white">
+                            Todos:
+                        </h1>
+                        <div className="flex space-x-8 items-center">
+                            {buttonFunctions.map((value, index) => (
+                                <div
+                                    key={index}
+                                    className="flex space-x-2 items-center"
+                                >
+                                    <div className={`w-2 h-2 ${value.color}`} />
+                                    <p>{value.name}</p>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                    <div className="border rounded-md p-2 space-y-2">
+                        {dummyTodoItems.map(
+                            (value, index) =>
+                                !value.completed && (
+                                    <div
+                                        key={value.id}
+                                        className="flex bg-white/10 p-4 rounded-sm justify-between"
+                                    >
+                                        <div className="flex space-x-2">
+                                            <h1>Task {index + 1}:</h1>
+                                            <h1 key={value.id}>{value.task}</h1>
+                                        </div>
+                                        <div className="flex items-center justify-center space-x-2">
+                                            {buttonFunctions.map((value, i) => (
+                                                <div
+                                                    key={value.name}
+                                                    className={`h-4 aspect-square rounded-sm hover:scale-105 transition-all cursor-pointer ${value.color}`}
+                                                />
+                                            ))}
+                                        </div>
+                                    </div>
+                                )
+                        )}
+                    </div>
+                </div>
+                <div className="border p-4 rounded-xl space-y-4">
+                    <h1 className="text-2xl font-bold text-white">
+                        Completed Recently:
+                    </h1>
+                    <div className="border rounded-md p-2">
+                        {dummyTodoItems.map(
+                            (value) =>
+                                value.completed && (
+                                    <h1 key={value.id}>{value.task}</h1>
+                                )
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
